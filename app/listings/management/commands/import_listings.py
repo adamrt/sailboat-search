65e6a5068e99f6e3f1e7b220a68d5a7abed56953
Adam Patterson <adam@adamrt.com>
2017-06-08 17:13:06 -0700

    remove imports

--- app/listings/management/commands/import_listings.py
+++ app/listings/management/commands/import_listings.py
@@ -3,9 +3,7 @@ from difflib import SequenceMatcher
 from django.core.management.base import BaseCommand, CommandError
 
 from boats.models import Boat
-from boats.scrapers import BoatScraper
 from listings.models import Listing
-from listings.scrapers import ListingScraper
 
 class Command(BaseCommand):
     help = 'Import all boats from bluewater'
