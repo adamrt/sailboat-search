51e7ddc770d188708155f976ecc44412689a762c
Adam Patterson <adam@adamrt.com>
2017-06-08 17:21:05 -0700

    improve boat import

--- app/boats/management/commands/import_boats.py
+++ app/boats/management/commands/import_boats.py
@@ -5,26 +5,49 @@ from django.core.management.base import BaseCommand, CommandError
 from boats.models import Boat
 
 class Command(BaseCommand):
-    help = 'Import all boats from bluewater'
+    help = 'Import boat models from multiple sources'
 
-    def handle(self, *args, **options):
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    ignore_boats = [
+        "adams 13 metre", # meters
+        "gemini 105m", # meters
+        "sea sprite 27/28", # length
+        "rhodes ranger 28/29" # length
+    ]
+
+    extra_boats = [
+        "Hunter 33.5",
+        "Hunter 35.5",
+        "Niagara 35",
+        "Catalina 27"
+    ]
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
+    @property
+    def bluewater_boats(self):
         url = "http://bluewaterboats.org/about/index/"
-        rq = requests.get(url, headers=headers)
+        rq = requests.get(url, headers=self.headers)
         soup = BeautifulSoup(rq.content, 'html.parser')
         boats = soup.select('div.boatimage')
-        for boat in boats:
-            name = boat.select('a')[0]['title']
-            Boat.objects.get_or_create(name=name)
+        return [boat.select('a')[0]['title'] for boat in boats]
 
+    @property
+    def atom_boats(self):
         url = "http://atomvoyages.com/planning/good-old-boats-list.html"
-        rq = requests.get(url, headers=headers)
+        rq = requests.get(url, headers=self.headers)
         soup = BeautifulSoup(rq.content, 'html.parser')
         boats = soup.select('div.items-leading table ul li')
-        for boat in boats:
-            name = boat.select('a')[0].get_text()
-            Boat.objects.get_or_create(name=name)
+        return [boat.select('a')[0].get_text() for boat in boats]
 
-        for boat in Boat.objects.all():
-            boat.length = boat.length_from_name
-            boat.save()
+    def handle(self, *args, **options):
+        names = self.extra_boats + self.bluewater_boats + self.atom_boats
+        for name in names:
+
+            if name.lower() in self.ignore_boats:
+                continue
+
+            boat, created = Boat.objects.get_or_create(name=name)
+            if created or boat.length == None:
+                print("Adding: {}".format(name))
+                boat.length = boat.length_from_name
+                boat.save()
