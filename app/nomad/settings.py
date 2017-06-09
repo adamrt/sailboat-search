5ac6aa4e661d20dc84321cdd755275086437af07
Adam Patterson <adam@adamrt.com>
2017-06-08 17:11:37 -0700

    update requirements

--- app/nomad/settings.py
+++ app/nomad/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+
     'django_extensions',
+    'django_filters',
+    'widget_tweaks',
 
     'boats',
     'listings',
